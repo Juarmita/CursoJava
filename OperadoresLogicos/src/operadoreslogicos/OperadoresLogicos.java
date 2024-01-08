f67b4371e05439c186879cc30d0e9aaaa9df6f60
Juarmita <garciajurado@gmail.com>
2024-01-08 13:37:44 +0100

    Changes

--- OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -14,7 +14,8 @@ public class OperadoresLogicos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaracion de variables
+        
     }
     
 }
