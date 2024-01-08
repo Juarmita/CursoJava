6b62992381f0f6f869d66acb1eb166a5892232b8
Juarmita <garciajurado@gmail.com>
2024-01-08 10:12:41 +0100

    First upload

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author juanm
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaracion de tipo short, entero de 16 bits
+        short edad = 22; //Declaracion es tipo + nombre_variable = valor;
+        System.out.println(edad);
+        //Declaracion de tipo int, su valor por defecto es 0, y su uso es para los numeros naturales
+        int edad_int = 34;
+        System.out.println(edad_int);
+        //Declaracion de tipo long, ocupa 64 bits. Los tipo long, estan obligados a llevar la L al final
+        long dinero = 100l;
+        System.out.println(dinero);
+        //Declaracion de tipo char. Se necesitan comillas simples ('')
+        char letra = 'A';
+        System.out.println(letra);
+        //Declaracion de tipo booleano. El valor por defecto es false.
+        boolean isTrue = true;
+        System.out.println(isTrue);
+        //Declaracion de tipo float. Numeros decimales. Hay que indicar f en la declaracion
+        float precio = 128.54f;
+        System.out.println(precio);
+        float precio2 = 140f;
+        System.out.println(precio2);
+    }
+}
---
+++ datosPrimByte/src/datosprimbyte/DatosPrimByte.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datosprimbyte;
+
+/**
+ *
+ * @author juanm
+ */
+public class DatosPrimByte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte edad = 22; //Declaración del tipo byte. Tipo variable(byte)+nombre = valor
+        System.out.println(edad);
+        
+        
+    }
+    
+}
